7dec24876e770542a3b825ed11c8b807e6bebf92
T.Hanawa <jcc00456biz@gmail.com>
2023-10-08 23:22:30 +0900

    プロジェクト ファイルを追加します。

---
+++ Logger.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace MusicBeePlugin
+{
+    class Logger
+    {
+        private static Logger singleton = null;
+        private static int LOG_RETENTION_PERIOD = 30;
+        private string LogFilePath = null;
+        private string LogFileFileBase = null;
+        private string NowLogFileName = null;
+
+        private static string LOG_FILE_EXT = ".log";
+
+        private object LockObj = new object();
+        private StreamWriter stream = null;
+
+        public static Logger GetInstance(string log_file_path, string log_file_base )
+        {
+            if ( singleton == null )
+            {
+                singleton = new Logger( log_file_path, log_file_base );
+            }
+            return singleton;
+        }
+        private Logger( string log_file_path, string log_file_base )
+        {
+            // ログファイルの保存パス
+            this.LogFilePath = log_file_path;
+            this.LogFileFileBase = log_file_base;
+
+            // ログファイル名生成
+            string fname = string.Format( "{0}_{1}{2}", this.LogFileFileBase, DateTime.Now.ToString( "yyyy-MM-dd" ), LOG_FILE_EXT );
+            this.NowLogFileName = System.IO.Path.Combine( this.LogFilePath, fname );
+
+            // ログファイル作成
+            CreateLogfile( new FileInfo( this.NowLogFileName ) );
+        }
+        private void CreateLogfile( FileInfo logFile )
+        {
+            if ( !Directory.Exists( logFile.DirectoryName ) )
+            {
+                Directory.CreateDirectory( logFile.DirectoryName );
+            }
+
+            this.stream = new StreamWriter( logFile.FullName, true, Encoding.GetEncoding( "shift_jis" ) )
+            {
+                AutoFlush = true
+            };
+        }
+        public void LogOutput( string msg )
+        {
+            // ログファイル名生成
+            string fname = string.Format( "{0}_{1}{2}", this.LogFileFileBase, DateTime.Now.ToString( "yyyy-MM-dd" ), LOG_FILE_EXT );
+            string cmp_fname = System.IO.Path.Combine( this.LogFilePath, fname );
+            if ( this.NowLogFileName != cmp_fname )
+            {
+                this.stream.Close();
+                DeleteOldLogFile();
+                this.NowLogFileName = cmp_fname;
+                CreateLogfile( new FileInfo( this.NowLogFileName ) );
+            }
+            // ログ出力
+            string fullMsg = string.Format( "{0} {1}", DateTime.Now.ToString( "yyyy-MM-dd HH:mm:ss.fff" ), msg );
+            lock ( this.LockObj )
+            {
+                this.stream.WriteLine( fullMsg );
+            }
+        }
+        private void DeleteOldLogFile()
+        {
+            string fname_pattern = string.Format( "{0}_*{1}", this.LogFileFileBase, LOG_FILE_EXT );
+
+            // LOG_RETENTION_PERIOD日前の日付
+            DateTime retentionDate = DateTime.Today.AddDays( -LOG_RETENTION_PERIOD );
+
+            string[] filePathList = Directory.GetFiles( this.LogFilePath, fname_pattern, SearchOption.TopDirectoryOnly );
+            foreach ( string filePath in filePathList )
+            {
+                string work;
+                work = filePath.Replace( this.LogFilePath, "" );
+                work = work.Replace( this.LogFileFileBase + "_", "" );
+                work = work.Replace(LOG_FILE_EXT, "" );
+                DateTime logCreatedDate = DateTime.ParseExact( work, "yyyyMMdd", null );
+                if ( logCreatedDate < retentionDate )
+                {
+                    File.Delete( filePath );
+                }
+            }
+        }
+    }
+}
---
+++ TrackRename.cs
@@ -0,0 +1,264 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace MusicBeePlugin
+{
+    public partial class Plugin
+    {
+        private MusicBeeApiInterface mbApiInterface;
+        private PluginInfo about = new PluginInfo();
+
+        private string Menu0 = "context.Main/" + Properties.Resources.Menu0;
+        private string Menu0_0 = "context.Main/" + Properties.Resources.Menu0 + "/" + Properties.Resources.Menu0_0;
+        private string Menu0_1 = "context.Main/" + Properties.Resources.Menu0 + "/" + Properties.Resources.Menu0_1;
+
+        private Logger Mylog = null;
+
+        public PluginInfo Initialise(IntPtr apiInterfacePtr)
+        {
+            mbApiInterface = new MusicBeeApiInterface();
+            mbApiInterface.Initialise(apiInterfacePtr);
+            about.PluginInfoVersion = PluginInfoVersion;
+            about.Name = Properties.Resources.MyName;
+            about.Description = Properties.Resources.MyDescription;
+            about.Author = Properties.Resources.Author;
+            about.TargetApplication = "";   //  the name of a Plugin Storage device or panel header for a dockable panel
+            about.Type = PluginType.General;
+            about.VersionMajor = 1;  // your plugin version
+            about.VersionMinor = 0;
+            about.Revision = 1;
+            about.MinInterfaceVersion = MinInterfaceVersion;
+            about.MinApiRevision = MinApiRevision;
+            about.ReceiveNotifications = (ReceiveNotificationFlags.PlayerEvents | ReceiveNotificationFlags.TagEvents);
+            about.ConfigurationPanelHeight = 0;   // height in pixels that musicbee should reserve in a panel for config settings. When set, a handle to an empty panel will be passed to the Configure function
+
+            mbApiInterface.MB_AddMenuItem( Menu0, null, null );
+            mbApiInterface.MB_AddMenuItem( Menu0_0, null, MyPlugin );
+            mbApiInterface.MB_AddMenuItem( Menu0_1, null, MyPlugin );
+
+            Mylog = Logger.GetInstance( mbApiInterface.Setting_GetPersistentStoragePath(), "TrackRename" );
+            return about;
+        }
+        private void MyPlugin( object sender, EventArgs args )
+        {
+            // 生成中または一時停止中は処理しない
+            switch ( mbApiInterface.Player_GetPlayState() )
+            {
+                case PlayState.Playing:
+                case PlayState.Paused:
+                    // 再生・一時停止中は処理しない
+                    return;
+                default:
+                    break;
+            }
+
+            string a = args.ToString();
+            char delimiterChars;
+            if ( a == Menu0_0 )
+            {
+                delimiterChars = ',';
+            }
+            else
+            {
+                delimiterChars = '\t';
+            }
+
+
+            string fname = SelectFile();
+            if ( fname.Length != 0 )
+            {
+                StreamReader sr = new StreamReader( @fname, Encoding.GetEncoding("shift_jis") );
+                List<string> keys = new List<string>();
+                List<string> values = new List<string>();
+
+                while ( !sr.EndOfStream )
+                {
+                    string line = sr.ReadLine();
+                    string[] wk = line.Split( delimiterChars );
+                    keys.Add( wk[0].Trim() );
+                    values.Add( wk[1].Trim() );
+                }
+                // 選択中のものを取得
+                if ( !mbApiInterface.Library_QueryFilesEx( "domain=SelectedFiles", out string[] files ) )
+                    files = new string[0];
+
+                // 未選択の場合
+                if ( files.Length == 0 )
+                {
+                    return;
+                }
+                // 選択分の処理
+                string at;
+                string tt;
+                bool refresh_f = false;
+                string dataPath = mbApiInterface.Setting_GetPersistentStoragePath();
+                foreach ( string file in files )
+                {
+                    at = mbApiInterface.Library_GetFileTag( file, MetaDataType.Album );
+                    tt = mbApiInterface.Library_GetFileTag( file, MetaDataType.TrackTitle );
+                    // キーで検索
+                    for ( int i = 0; i < keys.Count; i++ )
+                    {
+                        if ( tt == keys[i] && values[i].Length != 0 )
+                        {
+                            mbApiInterface.Library_SetFileTag( file, MetaDataType.TrackTitle, values[i] );
+                            mbApiInterface.Library_CommitTagsToFile( file );
+                            // Log
+                            string log_str = string.Format( "[{0}][{1}]=>[{2}]", at, tt, values[i] );
+                            Mylog.LogOutput( log_str );
+                            refresh_f = true;
+                            break;
+                        }
+                    }
+
+                }
+                if ( refresh_f == true )
+                {
+                    mbApiInterface.MB_RefreshPanels();
+                }
+            }
+        }
+        private string SelectFile()
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.FileName = "*.*";
+            ofd.Filter = Properties.Resources.Filter + "(*.*)|*.*";
+            ofd.FilterIndex = 2;
+            ofd.Title = Properties.Resources.OpenFileTitle;
+            ofd.RestoreDirectory = true;
+
+            if ( ofd.ShowDialog() == DialogResult.OK )
+            {
+                return ( ofd.FileName );
+            }
+            return ("");
+        }
+        public bool Configure(IntPtr panelHandle)
+        {
+            // save any persistent settings in a sub-folder of this path
+            string dataPath = mbApiInterface.Setting_GetPersistentStoragePath();
+            // panelHandle will only be set if you set about.ConfigurationPanelHeight to a non-zero value
+            // keep in mind the panel width is scaled according to the font the user has selected
+            // if about.ConfigurationPanelHeight is set to 0, you can display your own popup window
+            if (panelHandle != IntPtr.Zero)
+            {
+                Panel configPanel = (Panel)Panel.FromHandle(panelHandle);
+                Label prompt = new Label();
+                prompt.AutoSize = true;
+                prompt.Location = new Point(0, 0);
+                prompt.Text = "prompt:";
+                TextBox textBox = new TextBox();
+                textBox.Bounds = new Rectangle(60, 0, 100, textBox.Height);
+                configPanel.Controls.AddRange(new Control[] { prompt, textBox });
+            }
+            return false;
+        }
+       
+        // called by MusicBee when the user clicks Apply or Save in the MusicBee Preferences screen.
+        // its up to you to figure out whether anything has changed and needs updating
+        public void SaveSettings()
+        {
+            // save any persistent settings in a sub-folder of this path
+            string dataPath = mbApiInterface.Setting_GetPersistentStoragePath();
+        }
+
+        // MusicBee is closing the plugin (plugin is being disabled by user or MusicBee is shutting down)
+        public void Close(PluginCloseReason reason)
+        {
+        }
+
+        // uninstall this plugin - clean up any persisted files
+        public void Uninstall()
+        {
+        }
+
+        // receive event notifications from MusicBee
+        // you need to set about.ReceiveNotificationFlags = PlayerEvents to receive all notifications, and not just the startup event
+        public void ReceiveNotification(string sourceFileUrl, NotificationType type)
+        {
+            // perform some action depending on the notification type
+            switch (type)
+            {
+                case NotificationType.PluginStartup:
+                    // perform startup initialisation
+                    switch (mbApiInterface.Player_GetPlayState())
+                    {
+                        case PlayState.Playing:
+                        case PlayState.Paused:
+                            // ...
+                            break;
+                    }
+                    break;
+                case NotificationType.TrackChanged:
+                    string artist = mbApiInterface.NowPlaying_GetFileTag(MetaDataType.Artist);
+                    // ...
+                    break;
+            }
+        }
+
+        // return an array of lyric or artwork provider names this plugin supports
+        // the providers will be iterated through one by one and passed to the RetrieveLyrics/ RetrieveArtwork function in order set by the user in the MusicBee Tags(2) preferences screen until a match is found
+        //public string[] GetProviders()
+        //{
+        //    return null;
+        //}
+
+        // return lyrics for the requested artist/title from the requested provider
+        // only required if PluginType = LyricsRetrieval
+        // return null if no lyrics are found
+        //public string RetrieveLyrics(string sourceFileUrl, string artist, string trackTitle, string album, bool synchronisedPreferred, string provider)
+        //{
+        //    return null;
+        //}
+
+        // return Base64 string representation of the artwork binary data from the requested provider
+        // only required if PluginType = ArtworkRetrieval
+        // return null if no artwork is found
+        //public string RetrieveArtwork(string sourceFileUrl, string albumArtist, string album, string provider)
+        //{
+        //    //Return Convert.ToBase64String(artworkBinaryData)
+        //    return null;
+        //}
+
+        //  presence of this function indicates to MusicBee that this plugin has a dockable panel. MusicBee will create the control and pass it as the panel parameter
+        //  you can add your own controls to the panel if needed
+        //  you can control the scrollable area of the panel using the mbApiInterface.MB_SetPanelScrollableArea function
+        //  to set a MusicBee header for the panel, set about.TargetApplication in the Initialise function above to the panel header text
+        //public int OnDockablePanelCreated(Control panel)
+        //{
+        //  //    return the height of the panel and perform any initialisation here
+        //  //    MusicBee will call panel.Dispose() when the user removes this panel from the layout configuration
+        //  //    < 0 indicates to MusicBee this control is resizable and should be sized to fill the panel it is docked to in MusicBee
+        //  //    = 0 indicates to MusicBee this control resizeable
+        //  //    > 0 indicates to MusicBee the fixed height for the control.Note it is recommended you scale the height for high DPI screens(create a graphics object and get the DpiY value)
+        //    float dpiScaling = 0;
+        //    using (Graphics g = panel.CreateGraphics())
+        //    {
+        //        dpiScaling = g.DpiY / 96f;
+        //    }
+        //    panel.Paint += panel_Paint;
+        //    return Convert.ToInt32(100 * dpiScaling);
+        //}
+
+        // presence of this function indicates to MusicBee that the dockable panel created above will show menu items when the panel header is clicked
+        // return the list of ToolStripMenuItems that will be displayed
+        //public List<ToolStripItem> GetHeaderMenuItems()
+        //{
+        //    List<ToolStripItem> list = new List<ToolStripItem>();
+        //    list.Add(new ToolStripMenuItem("A menu item"));
+        //    return list;
+        //}
+
+        //private void panel_Paint(object sender, PaintEventArgs e)
+        //{
+        //    e.Graphics.Clear(Color.Red);
+        //    TextRenderer.DrawText(e.Graphics, "hello", SystemFonts.CaptionFont, new Point(10, 10), Color.Blue);
+        //}
+
+    }
+}
